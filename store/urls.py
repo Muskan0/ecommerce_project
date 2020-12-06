162c43103206205ebc7eafb82ac88fae5355bcab
MUSKAN KALRA <muskankalra001@gmail.com>
2020-12-07 00:09:18 +0530

    Setup and created templates

---
+++ store/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.store, name='store'),
+    path('cart/',views.cart, name='cart'),
+    path('checkout/',views.checkout, name='checkout'),
+]
