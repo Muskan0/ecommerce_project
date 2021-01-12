06a81da5692f7dad20af95eb4e287f1d1b0e6d07
MUSKAN KALRA <muskankalra001@gmail.com>
2021-01-12 23:27:09 +0530

    User authentication and login, logout feature

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('update_item/', views.updateItem, name='update_item'),
--- store/views.py
+++ store/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import JsonResponse
 import json
 import datetime
 from .utils import cookieCart, cartData, guestOrder
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('store')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'store/register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+
+    context = {}
+    return render(request, 'store/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('store')
+
 
 def store(request):
     data = cartData(request)
